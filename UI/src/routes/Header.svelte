e3c3acc78b9d01498387778243e1ff9da7926b9b
LeonBeltran <99263761+LeonBeltran@users.noreply.github.com>
2024-05-19 06:30:06 +0000

    Added redirect buttons to the header

--- UI/src/routes/Header.svelte
+++ UI/src/routes/Header.svelte
@@ -19,11 +19,14 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/EventList' ? 'page' : undefined}>
+				<a href="/EventList">Events</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname.startsWith('/CreateEvent') ? 'page' : undefined}>
+				<a href="/CreateEvent">Add Event</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/ResellTicket') ? 'page' : undefined}>
+				<a href="/ResellTicket">Resell Tickets</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
